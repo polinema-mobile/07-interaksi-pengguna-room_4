b0edb6bcf47961cc0905ceb788b4424e2eb9607d
dinariskyas <dinarisky04@gmail.com>
2020-09-29 13:48:40 +0700

    menambahkan datepicker, edit values pada strings.xml

--- app/src/main/java/com/dina/MainActivity.java
+++ app/src/main/java/com/dina/MainActivity.java
@@ -2,13 +2,42 @@ package com.dina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtTanggalLahir;
+    Calendar calendar;
+    DatePickerDialog picker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtTanggalLahir = (EditText) findViewById(R.id.edtTanggalLahir);
+
+        edtTanggalLahir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar= Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                // date picker dialog
+                picker=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        edtTanggalLahir.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                    }
+                }, year, month, day);
+                picker.show();
+            }
+        });
     }
 }
